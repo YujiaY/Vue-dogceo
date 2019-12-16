705181b7e689412335ebe26b07aee351a916eb88
YujiaY <jack.yujia.yuan@gmail.com>
2019-12-16 11:58:10 +1000

    Use axios.all to ensure the sequence of api results.

--- vueApp.js
+++ vueApp.js
@@ -8,7 +8,6 @@ const vm = new Vue({
       allBreeds: [],
       selectedBreeds: [],
       maxSelection: 2,
-      picDivs: [],
       picsUrls: {},
       maxPic: 10
     }
@@ -17,16 +16,12 @@ const vm = new Vue({
   created: function () {
     this.getAllBreeds();
   },
+
   methods: {
     removeElement(breedIndex, urlIndex) {
-      console.log(breedIndex)
-      console.log(urlIndex)
-      // console.log(this.picsUrls)
-      // console.log(this.picsUrls(0))
       this.picsUrls[breedIndex].urlArr.splice(urlIndex, 1);
       console.log(this.picsUrls[breedIndex].urlArr.length)
-      if (this.picsUrls[breedIndex].urlArr.length ===0) {
-        // this.picsUrls.slice(breedIndex, 1);
+      if (this.picsUrls[breedIndex].urlArr.length === 0) {
         this.$delete(
           this.selectedBreeds,
           this.selectedBreeds.indexOf(this.picsUrls[breedIndex].breedName)
@@ -40,38 +35,26 @@ const vm = new Vue({
         this.allBreeds = (response.data.message)
       })
     },
-    getRandomImgsForEachBreed(breedName, number) {
-      let name = breedName;
-      axios.get(photoBaseUrl + breedName + '/images/random/' + number )
-        .then((res) => {
-          let data = res.data.message
-          let obj = {
-            breedName,
-            urlArr: res.data.message
-          }
-          // console.log(breedName)
-          // console.log(data)
-          // console.log(this.picsUrls)
-          //working
-          this.picsUrls.push(obj);
-          
-          // not working
-          // Vue.set(this.picsUrls, breedName, { urlArr: data });
-          console.log(this.picsUrls)
-
-          // this.$set(this.testNew, breedName, { urlArr: data });
-          // this.$set(this.picsUrls, breedName, { urlArr: res.data.message });
-          // console.log(this.picsUrls);
-      });
+    getImgsPerBreed: function (breedName, number) {
+      return axios.get(photoBaseUrl + breedName + '/images/random/' + number)
     },
-    getRamdomImages: function () {
+    getImgsForAll: function () {
       let numberPerBreed = this.maxPic / this.selectedBreeds.length;
-      this.picDivs = [];
       this.picsUrls = [];
+      let axiosQueries = [];
       this.selectedBreeds.forEach((breed) => {
-        this.getRandomImgsForEachBreed(breed, numberPerBreed)
-      })
-    }
-
-  }
-});
+        axiosQueries.push(this.getImgsPerBreed(breed, numberPerBreed))
+      });
+      axios.all(axiosQueries)
+        .then(resArr => {
+          resArr.forEach((res, index) => {
+            let breedName = this.selectedBreeds[index];
+            let obj = {
+              breedName,
+              urlArr: res.data.message
+            }
+            this.picsUrls.push(obj);
+          })
+        })
+    }}
+  });
